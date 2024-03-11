bbb0612a5489a50be08f81edf6376907665b1876
hangwl <hangwei54e@hotmail.com>
2024-03-11 10:02:45 +0800

    part 1 - component state, event handling

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,57 +1,15 @@
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Content = ({ course }) => {
-  const { parts } = course;
-
-  const partComponents = parts.map((part, index) => (
-    <Part key={index} part={part} />
-  ));
-
-  return <div>{partComponents}</div>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Total = ({ course }) => {
-  const totalExercises = course.parts.reduce(
-    (total, part) => total + part.exercises,
-    0
-  );
-  return <p>Number of exercises {totalExercises}</p>;
-};
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const [counter, setCounter] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
     </div>
   );
 };
