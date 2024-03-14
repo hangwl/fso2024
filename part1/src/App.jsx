7cdd4b5e3ef80e712f7866b22a62e7c6c4aba20b
hangwl <hangwei54e@hotmail.com>
2024-03-15 02:33:40 +0800

    part2 collection rendering and modules

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,62 +1,17 @@
-import { useState } from "react";
+import Note from "./components/Note"
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
-
-const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
-  ];
-
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
-  const [mostVotedIndex, setMostVotedIndex] = useState(0);
-
-  const handleVote = () => {
-    const newVotes = [...votes];
-    newVotes[selected]++;
-    setVotes(newVotes);
-    updateMostVotedIndex(newVotes);
-  };
-
-  const handleNextAnecdote = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length));
-  };
-
-  const updateMostVotedIndex = (newVotes) => {
-    let maxIndex = 0;
-    for (let i = 1; i < newVotes.length; i++) {
-      if (newVotes[i] > newVotes[maxIndex]) {
-        maxIndex = i;
-      }
-    }
-    setMostVotedIndex(maxIndex);
-  };
+const App = ({ notes }) => {
 
   return (
     <div>
-      <div>
-        <h2>Anecdote of the Day</h2>
-        <p>{anecdotes[selected]}</p>
-        <p>Has {votes[selected]} votes</p>
-        <Button handleClick={handleVote} text="Vote" />
-        <Button handleClick={handleNextAnecdote} text="Next Anecdote" />
-      </div>
-      <div>
-        <h2>Top Voted Anecdote</h2>
-        <p>{anecdotes[mostVotedIndex]}</p>
-        <p>Has {votes[mostVotedIndex]} votes</p>
-      </div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
