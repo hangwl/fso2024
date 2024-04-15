e37d8804cb6e9fbfa632e0898f2945729661d33c
hangwl <hangwei54e@hotmail.com>
2024-04-15 16:42:43 +0800

    part3 - deploying note app using flyio

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors());
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -88,7 +89,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
