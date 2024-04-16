0db34418222beb8eb97c89475bc81b73ae8e1064
hangwl <hangwei54e@hotmail.com>
2024-04-16 13:14:40 +0800

    push frontend production build to backend repo

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
+app.use(express.static('dist'))
 app.use(express.json());
 app.use(cors());
 app.use(
@@ -104,7 +105,7 @@ app.use((req, res, next) => {
     res.status(404).send('404 - Not Found');
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
