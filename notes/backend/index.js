aecf76a45ca208e9efaa809051ce1e17b99387cf
hangwl <hangwei54e@hotmail.com>
2024-04-19 00:25:28 +0800

    Integrated mongodb with FE and BE for notesApp

--- notes/backend/index.js
+++ notes/backend/index.js
@@ -1,95 +1,105 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
 
-app.use(express.json())
+const Note = require("./models/note");
+
+app.use(express.static("dist"));
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const cors = require("cors");
+
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.json());
+app.use(requestLogger);
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-})
-
-app.put('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const updatedNote = request.body;
-
-    const index = notes.findIndex(note => note.id === id);
-    if (index !== -1) {
-        notes[index] = { ...notes[index], ...updatedNote };
-        response.json(notes[index]);
-    } else {
-        response.status(404).json({ error: 'Note not found' });
-    }
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-    response.status(204).end()
-})
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
 
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    }
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-    notes = notes.concat(note)
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-    response.json(note)
-})
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-const PORT = process.env.PORT || 3001
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- notes/backend/mongo.js
+++ notes/backend/mongo.js
@@ -1,39 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-    `mongodb+srv://hangwei123:${password}@cluster0.xwt8kxq.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://hangwei123:${password}@cluster0.xwt8kxq.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-mongoose.connect(url)
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
+  content: String,
+  important: Boolean,
+});
 
-const Note = mongoose.model('Note', noteSchema)
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-    content: 'HTML is easy',
-    important: true,
-})
+  content: "Callback-functions suck",
+  important: true,
+});
 
 // note.save().then(result => {
 //     console.log('note saved!')
 //     mongoose.connection.close()
 // })
 
-Note.find({ important: true }).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
-})
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
